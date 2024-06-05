8c9fc5c8695419349fa4b6b84f247a39a994fa41
Michael Oyinlola <moyinlola1972@gmail.com>
2024-06-05 13:15:26 +0100

    first commit

---
+++ Variable.py
@@ -0,0 +1,6 @@
+First_Name = "Michael"
+print(First_Name)
+Middle_Name = "Tunde"
+print(Middle_Name)
+Last_Name = "Oyinlola"
+print(Last_Name)
---
+++ VariousDataType.py
@@ -0,0 +1,27 @@
+# Integer Data Type
+a = 2
+print(a)
+print(type(a))
+# String Data Type
+First_Name = "Michael"
+print(First_Name)
+print(type(First_Name))
+#Boolean Data Type
+my_boolean = True
+print(my_boolean)
+
+my_List = [4, "Michael", 2.0 ]
+print(my_List)
+print(type(my_List))
+
+my_Tuple = (2,5,6,7,6)
+print(my_Tuple)
+print(type(my_Tuple))
+
+My_Dictionary = {"Name": "Michael", "Age": 25, "height": 5.2}
+print(My_Dictionary)
+print(type(My_Dictionary))
+
+My_Set = {5, "Orange", "Table"}
+print(My_Set)
+print(type(My_Set))
